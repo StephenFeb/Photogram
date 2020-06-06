c72a0214d0ade92cd9bb44ae26b190cf519309bb
StephenFeb <spinshon@hotmail.com>
2020-06-06 23:54:44 +0100

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,14 @@ class PostsController < ApplicationController
     def new
       @posts = Post.new
     end
+
+    def create
+        @posts = Post.create(post_params)
+        redirect_to posts_path
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:image, :caption)
+    end
 end
